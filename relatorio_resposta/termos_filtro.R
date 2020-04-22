8270084c141702f3b7c61b0f6559cbc120b3fc65
sbrt <sbrt@sbrt.us-east1-b.c.eighth-motif-274921.internal>
2020-04-22 17:04:15 +0000

    migração para o servidor

---
+++ relatorio_resposta/termos_filtro.R
@@ -0,0 +1,48 @@
+
+
+
+
+dir_proj<-"/home/micael/repositorios/github/textmining-sbrt/"
+setwd(dir_proj)
+source("scripts/respostas/get_funs.R")
+options(java.parameters="-Xmx2g")
+library(dfrtopics)
+library(tidytext)
+library(tidyverse)
+library(mallet)
+library(dendextend)
+library(readr)
+
+#Diretórios dos arquivos necessários
+respostas_dir<-"/home/micael/R_envs/text-mining/dados/sbrt_txts/respostas_txt"
+#respostas_dir<-"/home/micael/R_envs/text-mining/dados/sbrt_txts/respostas_txt"
+metadados_dir<-"dados/metadados_RT.csv"
+stop_words_sbrt<-"dados/stop_words_sbrt.txt"
+
+ids<-
+
+docs_interesse<-paste0(respostas_dir,ids,".txt")
+
+#Leitura das stopwords.
+sw<-readr::read_lines(stop_words_sbrt)
+
+#Leitura e pré-processamento dos documentos.
+txtdf<-get_txt(docs_interesse)
+
+#Criação de um dataframe dos metadados dos documentos.
+metadados<-readr::read_csv2(metadados_dir)
+
+#Preaparação dos documentos para criação do modelo LDA.
+instance_mallet<-make_instances(txtdf, stoplist_file = stop_words_sbrt)
+
+
+#Criação do modelo LDA de acordo com o número de tópicos encontrado no processo anterior.
+best_model<-train_model(instance_mallet,n_topics = 1, metadata = metadados, seed = 12345)
+best_model$doc_ids<-doc_ids(best_model)
+
+
+#Gera dataframe de termos por tópico.
+terms_sbrt_filtered<-tidy(best_model$model, matrix = "beta")%>%
+  arrange(desc(beta))
+
+
