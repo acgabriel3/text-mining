11e36172f3d635d7ab1e5d06ea3ff5f47f947fb2
Fernando F. Cordeiro <37345898+FernandoCordeiro@users.noreply.github.com>
2020-04-25 19:58:43 -0300

    Update main.py
    atualizacao do chatbot

--- src/main.py
+++ src/main.py
@@ -1,20 +1,48 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import math
+import json
 import logging
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
-# Enable info level logging
 logging.basicConfig(level=logging.INFO)
 
-chatbot = ChatBot('Bot')
+data = json.loads(open('dialogo.json', 'r').read())
 
-# Start by training our bot with the ChatterBot corpus data
-trainer = ChatterBotCorpusTrainer(chatbot)
+train = []
+tag = []
 
-trainer.train('chatterbot.corpus.portuguese')
+for row in data:
+    train.append(row['question'])
+    train.append(row['answer'])
 
-while True:
+for row in data:
+    tag.append(row['flag'])
+
+chatbot = ChatBot('Sara')
+
+trainer = ListTrainer(chatbot)
+trainer.train(train)
+
+while True: 
     try:
-        response = chatbot.get_response(input('Usuário: '))
-        print(f'{chatbot.name}: {response}')
-    except:
+        question = input('Usuário: ')
+        answer = chatbot.get_response(question.lower())
+        txt = str(answer)
+        
+        idx = math.floor(train.index(txt) / 2)
+
+        query = False
+        for i, val in enumerate(tag):
+            if val == 'consulta': 
+                query = True
+                break
+            if i == idx: break
+
+        if query == True:
+            print('Consulta')                               ##### Conectar ao elasticsearch ##### 
+        else:
+           print(f'{chatbot.name}: {answer}')
+    
+    except(KeyboardInterrupt, EOFError, SystemExit):
         break
+
