26033a7169eea4a8f591ae3df20689d4b795492d
Yuri Serka <yserka@hotmail.com>
2020-04-26 23:50:14 +0000

    só uns ajustes basicos

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import logging
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from elasticsearch import Elasticsearch
+from src.data.paths import dialogos_basicos_path, solicitacoes_path
 
 es = Elasticsearch()
 
@@ -19,13 +20,15 @@ chatbot = ChatBot(
 )
 trainer = ListTrainer(chatbot)
 
+trainer.train('chatterbot.corpus.portuguese')
+
 
 def train_from_elasticsearch_response(q, top_words=None):
     res = es.search(index="sentencas_sbrt", body={
         "query": {
             "match": {
                 "sentenca": {
-                    "query": f'{q} {top_words}'
+                    "query": q
                 }
             }
         }
@@ -34,34 +37,29 @@ def train_from_elasticsearch_response(q, top_words=None):
     if _DEV:
         print([hit['_source']['sentenca'] for hit in res['hits']['hits']])
 
-    l = []
     for hit in res['hits']['hits']:
-        l.append(q)
-        l.append(hit['_source']['sentenca'])
-
-    trainer.train(l)
+        if len(hit['_source']['sentenca'].split()) > 1:
+            trainer.train([q, hit['_source']['sentenca']])
 
 
-def train_from_builtin_data():
-    data = json.loads(open('dialogo.json', 'r').read())
+def train_from_builtin_data(path):
+    data = json.loads(open(path, 'r').read())
     train = []
-    tag = []
     for row in data:
         train.append(row['question'])
         train.append(row['answer'])
-    for row in data:
-        tag.append(row['flag'])
     trainer.train(train)
 
 
 def main():
-    train_from_builtin_data()
+    train_from_builtin_data(dialogos_basicos_path)
+    # train_from_builtin_data(solicitacoes_path) # tem as respostas em branco
     while True:
         try:
             question = input('Usuário: ')
             answer = chatbot.get_response(question.lower())
-            
-            query = False if answer.confidence > .7 else True
+
+            query = False if answer.confidence >= .5 else True
             if query:
                 # pegar as top_words?
                 train_from_elasticsearch_response(question)
